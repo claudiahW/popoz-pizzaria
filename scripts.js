718183237d4adb8c93d63d7f2b2fea7098b1b2bd
claudiahW <murungiclaud@gmail.com>
2021-08-04 14:36:39 +0300

    add js code to allow calculation

--- scripts.js
+++ scripts.js
@@ -3,4 +3,27 @@ function getSize() {
     var size = document.getElementById("pizzaSize").value;
     return parseInt(size);
   }
+  function getCrust() {
+    var crust = document.getElementById("pizzaCrust").value;
+    return parseInt(crust);
+  }
   
+  function getTopping() {
+    var topping = document.getElementById("pizzaTopping").value;
+    return parseInt(topping);
+  }
+  function getQuantity() {
+    var number = document.getElementById("quantity").value;
+    return parseInt(number);
+  }
+ 
+  function totalAmount() {
+      var totals = (getSize() + getCrust() + getTopping()) * getQuantity();
+      alert(
+      "Your order of"  +
+       getQuantity ("") +
+      "Pizza," +
+      "" +  "total to Shillings " + 
+       totals +
+      "" + ""+ ".Thank you for shopping with us!See you soon.")
+  }
