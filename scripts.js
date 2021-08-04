d81087e55247362a02a55ed7a596b11902cb0bd1
claudiahW <murungiclaud@gmail.com>
2021-08-04 14:01:09 +0300

    add README file

--- scripts.js
+++ scripts.js
@@ -2,8 +2,15 @@ $.button(function(event) {
     if ($("input").val() === "") {
         alert('Enter valid data')
         return;
+      
     }
+    {
 
     alert('Thank you for your order!')
-    event.preventDefault();
+    event.preventDefault("yes");
     this.reset();
+
+    alert('Thank you for your order!')
+    event.preventDefault("no");
+    this.reset();
+}
